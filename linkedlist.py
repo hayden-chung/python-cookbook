f8c4c252a0f8ee348048cf03cbab8d589b18ee3b
Hayden Chung <haydenchung1289@gmail.com>
2023-07-29 12:18:42 +1200

    Create linkedlist.py

---
+++ linkedlist.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        pass
+    
+    def append_item(self):
+        pass
+
+    def print(self): 
+        pass

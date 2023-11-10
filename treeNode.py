34c628200e5e171c38764fc1c58a1c43f01be8bd
Hayden Chung <haydenchung1289@gmail.com>
2023-11-11 10:18:26 +1300

    treenode

--- treeNode.py
+++ treeNode.py
@@ -1,12 +1,34 @@
-class TreeNode:
+class Node:
     def __init__(self, value):
-        self.value = value
+        self.val = value
         self.left = None
         self.right = None
 
-
 def insert(root, key):
-    pass
+    if root is None: 
+        return Node(key)
+    
+    if key < root.val:
+        root.left = insert(root.left, key)
+    
+    if key > root.val: 
+        root.right = insert(root.right, key)
 
-r = TreeNode(50)
-r = insert(r, 10)
+    return root 
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val)
+        inorder_traversal(root.right)
+
+r = Node (50)
+r = insert(r, 30)
+r = insert(r, 20)
+r = insert(r, 40)
+r = insert(r, 70)
+r = insert(r, 60)
+r = insert(r, 80)
+
+inorder_traversal(r)
+        
